3c07079afe7a78f5256423a614c5c8607044e7f0
Slavian94 <125400432+Slavian94@users.noreply.github.com>
2023-11-06 15:35:32 +0200

    test

--- src/main/java/abstract_class_and_interface/transport/land_transport/Bus.java
+++ src/main/java/abstract_class_and_interface/transport/land_transport/Bus.java
@@ -1,6 +1,7 @@
 package abstract_class_and_interface.transport.land_transport;
 
 public class Bus extends LandTransport {
+    int a;
     public Bus(String name, String color, int numberOfPassenger, String kindOfFuel, String material, double speed,
                int numberOfWheels, String destinationOfTransport) {
         super(name, color, numberOfPassenger, kindOfFuel, material, speed, numberOfWheels, destinationOfTransport);
